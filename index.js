44d8dd39a0efd280a64d976f31a025532e34de07
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-12-13 14:33:07 -0300

    adicao da biblioteca dotenv para guardar as informacoes do email, criacao da funcao que permite o envio do email e a adicao da funcionalidade de criacao do arquivo contendo as informacoes dos produtos

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const puppeteer = require("puppeteer");
 const nodemailer = require("nodemailer");
-const arquivo = require("fs");
+const file = require("fs");
+require("dotenv").config();
 
-const url = "https://www.mercadolivre.com.br";
-const search = "controle ps4";
-let count = 0;
+async function enviaEmail(){
+    const emailAddress = process.env.emailAddress;
+    const emailPassword = process.env.emailPassword;
+    
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: { 
+            user: emailAddress, 
+            pass: emailPassword 
+        }
+    });
+
+    try{
+        const info = await transporter.sendMail({
+            from: `"Jao 👨🏽‍💻" <${emailAddress}>`,
+            to: "jvdecampos@hotmail.com",
+            subject: "teste",
+            text: "É só um teste"
+        });
+        console.log("Email enviado", info);
+    } catch(error){
+        console.error("Erro ao enviar o email", error);
+    };
+};
 
 (async () => {
+    const url = "https://www.mercadolivre.com.br";
+    const search = "controle ps4";
+    let count = 0;
+
+    file.writeFile("/produtos.txt", `Os ${search} no ${url}`, (error) => {
+        return error;
+    });
+    const editFile = file.createWriteStream("/produtos.txt", {encoding: "utf8"});
+    
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     
@@ -34,10 +67,16 @@ let count = 0;
         const name = await page.$eval(".ui-pdp-title", (element) => element.innerText);
         const price = await page.$eval(priceSelector, (element) => element.innerText);
         console.log(name, price);
+        
+        editFile.write(`${name} custa ${price}`, "utf8", (error) => {
+            return error;
+        })
 
         count++
     }
 
     await page.waitForTimeout(2000);
     await browser.close();
+    await enviaEmail();
+    editFile.end();
 })();
